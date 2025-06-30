1afb954172023bc4ad6cff9d49fbc9d2e60331b0
sowrab <sowrab.light@gmail.com>
2025-07-01 01:30:04 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is Sowrab'
+    return 'Hello, world! This is Sowrab. Editing again'
 
 if __name__ == '__main__':
     app.run()
